2e28a8f58626c27b87aa73974f79871ccc8fb465
TJ FAITH <tjbenbiz@gmail.oom>
2020-06-10 09:40:26 -0700

    dg

---
+++ Drag and Drop/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Drag_and_Drop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rect_MLButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rc = sender as Rectangle;
+            DataObject data = new DataObject(rc.Fill);
+            DragDrop.DoDragDrop(rc, data, DragDropEffects.Move);
+        }
+
+        private void Target_Drop(object sender, DragEventArgs e)
+        {
+            SolidColorBrush scb = (SolidColorBrush)e.Data.GetData(typeof(SolidColorBrush));
+            Target.Fill = scb;
+        }
+    }
+}
